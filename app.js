dd17f95ba2b7dbbfe3fce30fe92b90fd118be288
leonlubberdink <mail@leonlubberdink.com>
2024-05-15 10:36:21 +0200

    Finished practice app 1

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: "Finish the course and learn Vue!",
+      courseGoalA: "Finish the course and learn Vue!",
+      courseGoalB: "<h2>Master Vue and build great apps!</h2>",
       vueLink: "https://vuejs.org/",
     };
   },
@@ -9,9 +10,9 @@ const app = Vue.createApp({
     outputGoal() {
       const random = Math.random();
       if (random > 0.5) {
-        return "Learn Vue";
+        return this.courseGoalA;
       } else {
-        return "Master Vue";
+        return this.courseGoalB;
       }
     },
   },
