bf0938f007d60fa8bcf66ba0d80cfff4d97b7738
leonlubberdink <mail@leonlubberdink.com>
2024-05-15 10:27:48 +0200

    Added Random String by defining methods in createApp confog object

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const app = Vue.createApp({
       vueLink: "https://vuejs.org/",
     };
   },
+  methods: {
+    outputGoal() {
+      const random = Math.random();
+      if (random > 0.5) {
+        return "Learn Vue";
+      } else {
+        return "Master Vue";
+      }
+    },
+  },
 });
 
 app.mount("#user-goal");
